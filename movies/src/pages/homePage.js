a088b05e7fc34b098840baa8a1aed083f60487ac
Thomas Farrell <44026261+TF20083540@users.noreply.github.com>
2023-10-10 12:16:56 +0100

    Add to favourites logic completed; DUmmy favourites page - redone while fixing Filters; resubmit for clarity an after checking functionality.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -23,12 +23,12 @@ const MovieListPage = (props) => {
     else setGenreFilter(value);
   };
 
-    const addToFavorites = (movieId) => {
-      const updatedMovies = movies.map((m) =>
-        m.id === movieId ? { ...m, favorite: true } : m
-      );
-      setMovies(updatedMovies);
-    };
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
   useEffect(() => {
     fetch(
@@ -57,7 +57,8 @@ const MovieListPage = (props) => {
            genreFilter={genreFilter}
          />
         </Grid>
-        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />      </Grid>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+        </Grid>
     </Grid>
   );
 };
