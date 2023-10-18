ce6ced4627bddbfe98f9b61df7876fe7b46cb890
Thomas Farrell <44026261+TF20083540@users.noreply.github.com>
2023-10-18 11:59:38 +0100

    Added Upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
 
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
